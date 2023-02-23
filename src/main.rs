08a48a743c50a7ca9bb6cfa89d2659e14979b6e5
ICavlek <ivan.cavlek1403@gmail.com>
2023-02-23 06:55:51 +0100

    Refactored

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,15 @@ pub mod symmetric;
 
 use crate::symmetric::caesar_cipher::CaesarCipher;
 
-fn main() {
+/// An example of the Caesar cipher encryption and decryption algorithms.
+fn caesar_cipher_example() {
     let plain_text: &str = "XYZ";
     let caesar_cipher: CaesarCipher = CaesarCipher::new(3);
     let cipher_text: String = caesar_cipher.encrypt(plain_text);
     println!("{} -> {}", plain_text, cipher_text);
     println!("{} -> {}", cipher_text, caesar_cipher.decrypt(&cipher_text));
+}
+
+fn main() {
+    caesar_cipher_example();
 }
